4eef0a803f214d020f2889621c9ed3f84838cf75
Jake Gridley <101298474+jakegridley@users.noreply.github.com>
2022-11-29 10:28:38 -0700

    after meeting with TA

---
+++ Library.java
@@ -0,0 +1,73 @@
+package MusicPlayer.cs335;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Library {
+
+	private String name;
+	private ArrayList<Song> songs;
+	private String sortby;
+	
+	
+	public Library() {
+		
+		this.songs = new ArrayList<Song>();
+		this.name = "Main Library";
+		this.sortby = "Name";
+		
+	}
+	
+	public Library(String name) {
+		
+		this.name = name;
+		this.songs = new ArrayList<Song>();
+		this.sortby = "Name";
+		
+	}
+	
+	public ArrayList<Song> getSongs() {
+		return this.songs;
+	}
+	
+	public void setSortby(String s) {
+		this.sortby = s;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void addSong(Song s) {
+		songs.add(s);
+		
+		if (this.sortby.equals("Name")) {
+			this.sortByName();
+		} else if (this.sortby.equals("Artist")) {
+			this.sortByArtist();
+		} else if (this.sortby.equals("Date")) {
+			this.sortByDate();
+		} else {
+			System.out.println("INCORRECT SORTBY VALUE\n *INSTEAD SORTED BY NAME");
+			this.sortByName();
+		}
+		
+		
+	}
+	
+	public void sortByName() {
+		songs.sort(Comparator.comparing(Song::getName));
+	}
+	
+	public void sortByArtist() {
+		songs.sort(Comparator.comparing(Song::getArtist));
+	}
+	
+	public void sortByDate() {
+		songs.sort(Comparator.comparing(Song::getReleaseDate));
+	}
+	
+	
+	
+	
+}
---
+++ SystemInfo.java
@@ -0,0 +1,13 @@
+package MusicPlayer.cs335;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
